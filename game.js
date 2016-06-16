c435db4b641235cb09d8c9e303b4fbe097bcc6ac
Martin Butts <martinbutts@Martins-MacBook-Air.local>
2016-06-16 14:56:13 +1200

    add makeGreen event

--- game.js
+++ game.js
@@ -9,8 +9,7 @@ function start () {
 
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
-    // BIND YOUR EVENT LISTENERS HERE
-    // The first one is provided for you
+    dots [i].addEventListener('click', makeBlue)
     dots[i].addEventListener('contextmenu', makeGreen)
   }
 }
@@ -21,7 +20,10 @@ function makeGreen (evt) {
   updateCounts()
 }
 
-// CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
